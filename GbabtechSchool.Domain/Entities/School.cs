cbc595a4cf64cdb4c4e66b53c3a7632f04e3eaec
Abdulmalik Muhammad <makhirmjd@gmail.com>
2025-09-17 13:32:27 +0100

    Tenant DB Context

--- GbabtechSchool.Domain/Entities/School.cs
+++ GbabtechSchool.Domain/Entities/School.cs
@@ -1,7 +1,8 @@
 ﻿namespace GbabtechSchool.Domain.Entities;
 
-public class School : BaseEntity
+public class School
 {
+    public int Id { get; set; }
     public string Name { get; set; } = default!;
     public DateTimeOffset EstablishedDate { get; set; }
 }
